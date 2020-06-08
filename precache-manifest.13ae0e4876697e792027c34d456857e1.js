53065ab4c5b1ce3c35c2aea0ea5b4f81b5529fe2
Harsh Kumar Jha <hkjal1605@gmail.com>
2020-06-08 18:35:52 +0100

    Updates

---
+++ precache-manifest.13ae0e4876697e792027c34d456857e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9748e5c8e0be3a5030334973c6a52a1f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4e473a0fcced9b3ccdcd",
+    "url": "/monster-rolodex/static/css/main.87ccbfca.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e473a0fcced9b3ccdcd",
+    "url": "/monster-rolodex/static/js/main.04596d13.chunk.js"
+  },
+  {
+    "revision": "b46281a00ff77db46658",
+    "url": "/monster-rolodex/static/js/runtime-main.c3b95803.js"
+  }
+]);
